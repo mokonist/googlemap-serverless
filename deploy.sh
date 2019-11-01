05dc4ee4324dec9b5d06050cf7d8bf6516cacac3
mokocm <52817048+mokocm@users.noreply.github.com>
2019-11-01 19:57:43 +0900

    Fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 sam build &&
-sam package --s3-bucket BUCKET_NAME_HERE --output-template-file packaged.yaml  &&
-sam deploy --template-file packaged.yaml --stack-name location-stack  --capabilities CAPABILITY_IAM
+sam package --s3-bucket BUCKET_NAME_HERE --output-template-file template.yaml  &&
+sam deploy --template-file template.yaml --stack-name location-stack  --capabilities CAPABILITY_IAM
